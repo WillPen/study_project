f03c60b4c6b81fb0fe37aef1bea3e776cc54765b
WillPen <jxpeng2011@sina.com>
2018-09-17 21:56:03 +0800

    initial version

---
+++ USR/common.h
@@ -0,0 +1,14 @@
+#ifndef _COMMON_H//宏定义，定义文件名称
+#define _COMMON_H
+
+#include "stm8s.h"
+#include "math.h"
+
+
+
+void delay_ms(u16 nCount); //毫秒延时程序
+
+
+
+
+#endif //定义文件名称结束
---
+++ USR/led.h
@@ -0,0 +1,13 @@
+#ifndef _LED_H//宏定义，定义文件名称
+#define _LED_H
+
+#include "common.h"
+
+
+
+
+
+
+
+
+#endif //_LED_H
